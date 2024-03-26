a90b1625487b5f3ef0b3da757d8b9bf52b6e0804
Thibaut Lefrancois <thibaut.lefrancois@etudiant.univ-lr.fr>
2024-03-26 16:26:58 +0100

    backend(RoomEntity): Adding Annotation

--- sae4api/src/Entity/Room.php
+++ sae4api/src/Entity/Room.php
@@ -5,9 +5,19 @@ namespace App\Entity;
 use ApiPlatform\Metadata\ApiResource;
 use App\Repository\RoomRepository;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Serializer\Annotation\Groups;
+
+use ApiPlatform\Metadata\Get;
+use ApiPlatform\Metadata\GetCollection;
 
 #[ORM\Entity(repositoryClass: RoomRepository::class)]
-#[ApiResource]
+#[ApiResource(
+    operations:[
+        New Get(),
+        New GetCollection()
+    ],
+    normalizationContext: ['groups' => ['room:read']],
+)]
 class Room
 {
     #[ORM\Id]
@@ -16,16 +26,20 @@ class Room
     private ?int $id = null;
 
     #[ORM\Column(length: 15)]
+    #[Groups(['room:read'])]
     private ?string $name = null;
 
     #[ORM\Column]
+    #[Groups(['room:read'])]
     private ?int $floor = null;
 
     #[ORM\OneToOne(inversedBy: 'room', cascade: ['persist', 'remove'])]
+    #[Groups(['room:read'])]
     private ?AcquisitionSystem $acquisitionSystem = null;
 
     #[ORM\ManyToOne(inversedBy: 'rooms')]
     #[ORM\JoinColumn(nullable: false)]
+    #[Groups(['room:read'])]
     private ?Department $department = null;
 
     public function getId(): ?int
