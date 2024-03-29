2b5717b7109aa380b262ce2884999b13e21b979e
TheMathys <mathys.besnard@etudiant.univ-lr.fr>
2024-03-29 14:50:49 +0100

    feat(Comfort Index): Add comfort index based on values

--- sae4app/tailwind.config.js
+++ sae4app/tailwind.config.js
@@ -7,12 +7,16 @@ export default {
   theme: {
     extend: {
       colors: {
+        transparent: 'transparent',
+        current: 'currentColor',
+        'orange-light': '#ffeaa7',
+        'orange-dark': '#fdcb6e',
+        'yellow-light': '#fdcb6e',
+        'yellow-dark': '#facc15',
         'green-light': '#55efc4',
         'green-dark': '#00b894',
         'blue-light': '#BDDEFF',
         'blue-dark': '#0984E3',
-        'orange-light': '#ffeaa7',
-        'orange-dark': '#fdcb6e',
         'red-light': '#ff7675',
         'red-dark': '#d63031',
         'gray-light': '#636e72',
