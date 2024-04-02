1a71fd38ab036d5a3a400b6a1ef0f69eedd1504f
Thibaut Lefrancois <thibaut.lefrancois@etudiant.univ-lr.fr>
2024-04-02 17:30:57 +0200

    feat(staff-dashboard): adding a staff dashboard for alerts

--- sae4app/src/main.jsx
+++ sae4app/src/main.jsx
@@ -1,10 +1,11 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './components/App.jsx'
-import Header from './components/Header.jsx'
-import './styles/index.css'
-import {createBrowserRouter, RouterProvider} from "react-router-dom"
-import DetailRoom from './components/DetailRoom.jsx'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./components/App.jsx";
+import Header from "./components/Header.jsx";
+import "./styles/index.css";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import DetailRoom from "./components/DetailRoom.jsx";
+import AppStaff from "./components/AppStaff.jsx";
 
 const router = createBrowserRouter([
   {
@@ -13,18 +14,22 @@ const router = createBrowserRouter([
     children: [
       {
         path: "/",
-        element: <App />
+        element: <App />,
       },
       {
         path: "/room/:tag",
-        element: <DetailRoom />
+        element: <DetailRoom />,
+      },
+      {
+        path: "/staff",
+        element: <AppStaff />,
       },
     ],
   },
-])
+]);
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <RouterProvider router={router} />
-  </React.StrictMode>,
-)
+  </React.StrictMode>
+);
