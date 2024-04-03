17f13f8245bf44d54ccd8a906a06cc8f2df37562
Thibaut Lefrancois <thibaut.lefrancois@etudiant.univ-lr.fr>
2024-04-03 18:46:32 +0200

    fix(interval-tips): fixing tips interval before clear tips

--- sae4api/src/Controller/TipsController.php
+++ sae4api/src/Controller/TipsController.php
@@ -34,86 +34,87 @@ class TipsController extends AbstractController
                 return $this->json($room->getTips());
             }
         }
-        else
+        
+        // Vider la collection de conseils de la salle
+        $room->getTips()->clear();
+
+        $response = $httpClient->request('GET', "https://api.openweathermap.org/data/2.5/weather?q=La%20Rochelle&units=metric&appid=bef0f873bd6633be3fbd81bedb9a02be&lang=fr");
+        $externalTemp = $response->toArray()['main']['temp'];
+
+        $tips = array(
+            "temperature" => array(
+                "trop_chaud" => "Il fait chaud, ouvrez les fenêtres et coupez le chauffage s'il est allumé.",
+                "trop_froid" => "Il fait froid, envisagez d'augmenter le chauffage si possible ou couvrez-vous.",
+                "chaud_dehors_froid_dedans" => "Il fait plus chaud dehors, ouvrez les fenêtres.",
+                "chaud_dedans_froid_dehors" => "Pensez à baisser le chauffage, si déjà fait, ouvrez pendant quelques minutes les fenêtres.",
+                "chaud_dehors_chaud_dedans" => "Baissez les volets et faites un courant d'air ou utilisez des ventilateurs.",
+            ),
+            "humidite" => array(
+                "trop_humide" => "Utilisez un déshumidificateur.",
+                "trop_sec" => "Utilisez un humidificateur."
+            ),
+            "co2" => array(
+                "risque" => "Allumez les VMC et ouvrez les portes pour aérer.",
+            )
+        );
+
+        $tipsToApply = array();
+    
+        // ****************** Filtrer les conseils basés sur la température ******************
+
+        if ($temp < 18 && $externalTemp < 18) // Si il fait froid dehors et dedans
         {
-            $response = $httpClient->request('GET', "https://api.openweathermap.org/data/2.5/weather?q=La%20Rochelle&units=metric&appid=bef0f873bd6633be3fbd81bedb9a02be&lang=fr");
-            $externalTemp = $response->toArray()['main']['temp'];
-
-            $tips = array(
-                "temperature" => array(
-                    "trop_chaud" => "Il fait chaud, ouvrez les fenêtres et coupez le chauffage s'il est allumé.",
-                    "trop_froid" => "Il fait froid, envisagez d'augmenter le chauffage si possible ou couvrez-vous.",
-                    "chaud_dehors_froid_dedans" => "Il fait plus chaud dehors, ouvrez les fenêtres.",
-                    "chaud_dedans_froid_dehors" => "Pensez à baisser le chauffage, si déjà fait, ouvrez pendant quelques minutes les fenêtres.",
-                    "chaud_dehors_chaud_dedans" => "Baissez les volets et faites un courant d'air ou utilisez des ventilateurs.",
-                ),
-                "humidite" => array(
-                    "trop_humide" => "Utilisez un déshumidificateur.",
-                    "trop_sec" => "Utilisez un humidificateur."
-                ),
-                "co2" => array(
-                    "risque" => "Allumez les VMC et ouvrez les portes pour aérer.",
-                )
-            );
-
-            $tipsToApply = array();
+            $tipsToApply[] = new Tip($tips['temperature']['trop_froid'], $currentDateTime, false, $room);
+        } 
         
-            // ****************** Filtrer les conseils basés sur la température ******************
-
-            if ($temp < 18 && $externalTemp < 18) // Si il fait froid dehors et dedans
-            {
-                $tipsToApply[] = new Tip($tips['temperature']['trop_froid'], $currentDateTime, false, $room);
-            } 
-            
-            elseif ($temp < 18 && $externalTemp > 18) // Si il fait froid dedans et chaud dehors
-            {
-                $tipsToApply[] = new Tip($tips['temperature']['chaud_dehors_froid_dedans'], $currentDateTime, false, $room);            
-            } 
-            
-            elseif ($temp > 26 && $externalTemp < 18) // Si il fait chaud dedans et froid dehors
-            {
-                $tipsToApply[] = new Tip($tips['temperature']['chaud_dedans_froid_dehors'], $currentDateTime, false, $room);
-            } 
-            
-            elseif ($temp > 26 && $externalTemp > 26) // Si il fait chaud dehors et dedans
-            {
-                $tipsToApply[] = new Tip($tips['temperature']['chaud_dehors_chaud_dedans'], $currentDateTime, false, $room);
-            } 
-            
-            elseif ($temp > 26 && ($externalTemp > 18 && $externalTemp < 26)) // Si il fait chaud dedans et acceptable dehors
-            {
-                $tipsToApply[] = new Tip($tips['temperature']['trop_chaud'], $currentDateTime, false, $room);
-            }
-
-            // ****************** Filtrer les conseils basés sur l'humidité ******************
-
-            if ($hum < 30) // Si le taux d'humidité est inférieur à 30%
-            {
-                $tipsToApply[] = new Tip($tips['humidite']['trop_sec'], $currentDateTime, false, $room);
-            } 
-            
-            elseif ($hum > 70) // Si le taux d'humidité est supérieur à 70%
-            {
-                $tipsToApply[] = new Tip($tips['humidite']['trop_humide'], $currentDateTime, false, $room);
-            } 
+        elseif ($temp < 18 && $externalTemp > 18) // Si il fait froid dedans et chaud dehors
+        {
+            $tipsToApply[] = new Tip($tips['temperature']['chaud_dehors_froid_dedans'], $currentDateTime, false, $room);            
+        } 
         
-            // ****************** Filtrer les conseils basés sur le CO2 **********************
-
-            if ($co2 > 1000) // Si le taux de CO2 est supérieur à 1000 ppm
-            {
-                $tipsToApply[] = new Tip($tips['co2']['risque'], $currentDateTime, false, $room);
-            } 
-
-            // Parcourir les conseils à appliquer et les ajouter à la salle avec addTip
-
-            foreach ($tipsToApply as $tip) {
-                $room->addTip($tip);
-                $entityManager->persist($tip);
-            }
-
-            $entityManager->flush();
+        elseif ($temp > 26 && $externalTemp < 18) // Si il fait chaud dedans et froid dehors
+        {
+            $tipsToApply[] = new Tip($tips['temperature']['chaud_dedans_froid_dehors'], $currentDateTime, false, $room);
+        } 
+        
+        elseif ($temp > 26 && $externalTemp > 26) // Si il fait chaud dehors et dedans
+        {
+            $tipsToApply[] = new Tip($tips['temperature']['chaud_dehors_chaud_dedans'], $currentDateTime, false, $room);
+        } 
+        
+        elseif ($temp > 26 && ($externalTemp > 18 && $externalTemp < 26)) // Si il fait chaud dedans et acceptable dehors
+        {
+            $tipsToApply[] = new Tip($tips['temperature']['trop_chaud'], $currentDateTime, false, $room);
         }
 
+        // ****************** Filtrer les conseils basés sur l'humidité ******************
+
+        if ($hum < 30) // Si le taux d'humidité est inférieur à 30%
+        {
+            $tipsToApply[] = new Tip($tips['humidite']['trop_sec'], $currentDateTime, false, $room);
+        } 
+        
+        elseif ($hum > 70) // Si le taux d'humidité est supérieur à 70%
+        {
+            $tipsToApply[] = new Tip($tips['humidite']['trop_humide'], $currentDateTime, false, $room);
+        } 
+    
+        // ****************** Filtrer les conseils basés sur le CO2 **********************
+
+        if ($co2 > 1000) // Si le taux de CO2 est supérieur à 1000 ppm
+        {
+            $tipsToApply[] = new Tip($tips['co2']['risque'], $currentDateTime, false, $room);
+        } 
+
+        // Parcourir les conseils à appliquer et les ajouter à la salle avec addTip
+
+        foreach ($tipsToApply as $tip) {
+            $room->addTip($tip);
+            $entityManager->persist($tip);
+        }
+
+        $entityManager->flush();
         return $this->json($room->getTips());
+
     }
 }
--- sae4api/src/Entity/Tip.php
+++ sae4api/src/Entity/Tip.php
@@ -2,11 +2,17 @@
 
 namespace App\Entity;
 
+use ApiPlatform\Metadata\ApiResource;
 use App\Repository\TipRepository;
 use Doctrine\DBAL\Types\Types;
 use Doctrine\ORM\Mapping as ORM;
 
 #[ORM\Entity(repositoryClass: TipRepository::class)]
+#[ApiResource(
+    operations: [
+    ],
+)
+]
 class Tip
 {
     #[ORM\Id]
