2de660ffd9ef6340b04fd8c1642566e0f6932ba1
Thibaut Lefrancois <thibaut.lefrancois@etudiant.univ-lr.fr>
2024-04-03 18:11:15 +0200

    Update Tip.php

--- sae4api/src/Entity/Tip.php
+++ sae4api/src/Entity/Tip.php
@@ -2,13 +2,11 @@
 
 namespace App\Entity;
 
-use ApiPlatform\Metadata\ApiResource;
 use App\Repository\TipRepository;
 use Doctrine\DBAL\Types\Types;
 use Doctrine\ORM\Mapping as ORM;
 
 #[ORM\Entity(repositoryClass: TipRepository::class)]
-#[ApiResource]
 class Tip
 {
     #[ORM\Id]
