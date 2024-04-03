1e27f316f7c6f15a7213d9812b6d9cd1a440fbd7
Achille Dupont <achille.dupont@etudiant.univ-lr.fr>
2024-04-04 00:49:59 +0200

    fix (spinner) : fix logo

--- sae4app/src/components/Spinner.jsx
+++ sae4app/src/components/Spinner.jsx
@@ -1,12 +1,15 @@
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faCircleNotch } from "@fortawesome/free-solid-svg-icons";
 
 const Spinner = () => {
-    return (
-        <>
-            <FontAwesomeIcon icon={ faCircleNotch } className="animate-spin text-gray-dark size-12 m-4 text-center"/>
-        </>
-    )
-}
+  return (
+    <>
+      <FontAwesomeIcon
+        icon={faCircleNotch}
+        className="animate-spin text-gray-dark size-12 m-4 text-center"
+      />
+    </>
+  );
+};
 
-export default Spinner
+export default Spinner;
