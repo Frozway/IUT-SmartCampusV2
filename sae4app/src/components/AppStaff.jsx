ef0c20d402c4c195e9a3f19977a63367cddb7e34
Thibaut Lefrancois <thibaut.lefrancois@etudiant.univ-lr.fr>
2024-04-03 13:15:37 +0200

    Update AppStaff.jsx

--- sae4app/src/components/AppStaff.jsx
+++ sae4app/src/components/AppStaff.jsx
@@ -7,7 +7,9 @@ const AppStaff = () => {
   const [loggedIn, setLoggedIn] = useState(false);
 
   const handleLogin = async (e) => {
+    // Pour éviter le rechargement de la page lors de la soumission du formulaire et donc pour éviter de perdre les données saisies
     e.preventDefault();
+
     try {
       const response = await fetch("http://localhost:8000/api/login", {
         method: "POST",
@@ -16,6 +18,7 @@ const AppStaff = () => {
         },
         body: JSON.stringify({ username, password }),
       });
+
       if (response.ok) {
         setLoggedIn(true);
       } else {
@@ -33,63 +36,65 @@ const AppStaff = () => {
         <h1>Bienvenue sur la page du staff</h1>
       </div>
     );
+  } else {
+    return (
+      <div className="flex justify-center mt-40">
+        <form
+          onSubmit={handleLogin}
+          className="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4 w-full max-w-xs"
+        >
+          <h2 className="text-2xl font-bold mb-6 text-center">Connexion</h2>
+          <div className="mb-8">
+            <label
+              className="block text-gray-700 text-sm font-bold mb-2"
+              htmlFor="username"
+            >
+              Nom d'utilisateur:
+            </label>
+            <input
+              className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+              id="username"
+              type="text"
+              placeholder="Nom d'utilisateur"
+              value={username}
+              onChange={(e) => setUsername(e.target.value)}
+              required
+            />
+          </div>
+          <div className="mb-8">
+            <label
+              className="block text-gray-700 text-sm font-bold mb-2"
+              htmlFor="password"
+            >
+              Mot de passe:
+            </label>
+            <input
+              className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline"
+              id="password"
+              type="password"
+              placeholder="Mot de passe"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+              required
+            />
+          </div>
+          <div className="flex items-center justify-between">
+            <button
+              className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline w-full"
+              type="submit"
+            >
+              Se connecter
+            </button>
+            {error && (
+              <div className="text-red-500 text-sm text-center pl-4">
+                {error}
+              </div>
+            )}
+          </div>
+        </form>
+      </div>
+    );
   }
-
-  return (
-    <div className="flex justify-center mt-40">
-      <form
-        onSubmit={handleLogin}
-        className="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4 w-full max-w-xs"
-      >
-        <h2 className="text-2xl font-bold mb-6 text-center">Connexion</h2>
-        <div className="mb-8">
-          <label
-            className="block text-gray-700 text-sm font-bold mb-2"
-            htmlFor="username"
-          >
-            Nom d'utilisateur:
-          </label>
-          <input
-            className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-            id="username"
-            type="text"
-            placeholder="Nom d'utilisateur"
-            value={username}
-            onChange={(e) => setUsername(e.target.value)}
-            required
-          />
-        </div>
-        <div className="mb-8">
-          <label
-            className="block text-gray-700 text-sm font-bold mb-2"
-            htmlFor="password"
-          >
-            Mot de passe:
-          </label>
-          <input
-            className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline"
-            id="password"
-            type="password"
-            placeholder="Mot de passe"
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
-            required
-          />
-        </div>
-        <div className="flex items-center justify-between">
-          <button
-            className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline w-full"
-            type="submit"
-          >
-            Se connecter
-          </button>
-          {error && (
-            <div className="text-red-500 text-sm text-center pl-4">{error}</div>
-          )}
-        </div>
-      </form>
-    </div>
-  );
 };
 
 export default AppStaff;
